09fdba5fa1d75412939347eb652fb65557112fc0
Peter Rodriguez <peter.rodriguez@canada.ca>
2021-02-24 18:41:13 +0000

    assert...Equals deprecated for assert...Equal

--- test/pytest/iris2odimTests.py
+++ test/pytest/iris2odimTests.py
@@ -45,40 +45,40 @@ def validateAttributes(utest, obj, ref_obj):
                 utest.assertTrue(np.array_equal(attr, ref_attr))
             else:
                 #print aname, attr, ref_attr
-                utest.assertEquals(attr, ref_attr)
+                utest.assertEqual(attr, ref_attr)
 #        else: print aname
 
 def validateTopLevel(utest, obj, ref_obj):
-    utest.assertEquals(obj.source, ref_obj.source)
-    utest.assertEquals(obj.date , ref_obj.date)
-    utest.assertEquals(obj.time, ref_obj.time)
-    utest.assertAlmostEquals(obj.longitude, ref_obj.longitude, 12)
-    utest.assertAlmostEquals(obj.latitude, ref_obj.latitude, 12)
-    utest.assertAlmostEquals(obj.height, ref_obj.height, 12)
-    utest.assertAlmostEquals(obj.beamwidth, ref_obj.beamwidth, 12)
+    utest.assertEqual(obj.source, ref_obj.source)
+    utest.assertEqual(obj.date , ref_obj.date)
+    utest.assertEqual(obj.time, ref_obj.time)
+    utest.assertAlmostEqual(obj.longitude, ref_obj.longitude, 12)
+    utest.assertAlmostEqual(obj.latitude, ref_obj.latitude, 12)
+    utest.assertAlmostEqual(obj.height, ref_obj.height, 12)
+    utest.assertAlmostEqual(obj.beamwidth, ref_obj.beamwidth, 12)
     validateAttributes(utest, obj, ref_obj)
 
 
 def validateScan(utest, scan, ref_scan):
-    utest.assertEquals(scan.source, ref_scan.source)
-    utest.assertEquals(scan.date, ref_scan.date)
-    utest.assertEquals(scan.time, ref_scan.time)
-    utest.assertEquals(scan.startdate, ref_scan.startdate)
-    utest.assertEquals(scan.starttime, ref_scan.starttime)
-    utest.assertEquals(scan.enddate, ref_scan.enddate)
-    utest.assertEquals(scan.endtime, ref_scan.endtime)
-    utest.assertAlmostEquals(scan.longitude, ref_scan.longitude, 12)
-    utest.assertAlmostEquals(scan.latitude, ref_scan.latitude, 12)
-    utest.assertAlmostEquals(scan.height, ref_scan.height, 12)
-    utest.assertAlmostEquals(scan.beamwidth, ref_scan.beamwidth, 12)
-    utest.assertAlmostEquals(scan.elangle, ref_scan.elangle, 12)
-    utest.assertEquals(scan.nrays, ref_scan.nrays)
-    utest.assertEquals(scan.nbins, ref_scan.nbins)
-    utest.assertEquals(scan.a1gate, ref_scan.a1gate)
-    utest.assertEquals(scan.rscale, ref_scan.rscale)
-    utest.assertEquals(scan.rstart, ref_scan.rstart)
+    utest.assertEqual(scan.source, ref_scan.source)
+    utest.assertEqual(scan.date, ref_scan.date)
+    utest.assertEqual(scan.time, ref_scan.time)
+    utest.assertEqual(scan.startdate, ref_scan.startdate)
+    utest.assertEqual(scan.starttime, ref_scan.starttime)
+    utest.assertEqual(scan.enddate, ref_scan.enddate)
+    utest.assertEqual(scan.endtime, ref_scan.endtime)
+    utest.assertAlmostEqual(scan.longitude, ref_scan.longitude, 12)
+    utest.assertAlmostEqual(scan.latitude, ref_scan.latitude, 12)
+    utest.assertAlmostEqual(scan.height, ref_scan.height, 12)
+    utest.assertAlmostEqual(scan.beamwidth, ref_scan.beamwidth, 12)
+    utest.assertAlmostEqual(scan.elangle, ref_scan.elangle, 12)
+    utest.assertEqual(scan.nrays, ref_scan.nrays)
+    utest.assertEqual(scan.nbins, ref_scan.nbins)
+    utest.assertEqual(scan.a1gate, ref_scan.a1gate)
+    utest.assertEqual(scan.rscale, ref_scan.rscale)
+    utest.assertEqual(scan.rstart, ref_scan.rstart)
     for pname in ref_scan.getParameterNames():
-        utest.assertEquals(scan.hasParameter(pname), 
+        utest.assertEqual(scan.hasParameter(pname), 
                            ref_scan.hasParameter(pname))
         param = scan.getParameter(pname)
         ref_param = ref_scan.getParameter(pname)
@@ -122,7 +122,7 @@ class rb52odimTest(unittest.TestCase):
         self.assertTrue(new_rio.objectType is _rave.Rave_ObjectType_PVOL)
         pvol = new_rio.object
         ref_pvol = _raveio.open(self.REF_PVOL).object
-        self.assertEquals(pvol.getNumberOfScans(), ref_pvol.getNumberOfScans())
+        self.assertEqual(pvol.getNumberOfScans(), ref_pvol.getNumberOfScans())
         validateTopLevel(self, pvol, ref_pvol)
         for i in range(pvol.getNumberOfScans()):
             scan = pvol.getScan(i)
